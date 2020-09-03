1489b77caf86a7b76d73c03a444f2de4d78973ed
Loic Coyle <loic.coyle@hotmail.fr>
2020-09-03 17:40:12 +0200

    fix incorect argument order

--- tests/elements/test_dipole.py
+++ tests/elements/test_dipole.py
@@ -14,15 +14,15 @@ rho_dipole = l_dipole / angle_dipole
 
 class TestDipole(TestCase):
     def test_init(self):
-        dipole = Dipole(angle_dipole, rho_dipole)
+        dipole = Dipole(rho_dipole, angle_dipole)
         assert dipole.length == l_dipole
         assert dipole._m_h == None
         assert dipole._m_v == None
 
     def test_transfer_matrix(self):
-        dipole = Dipole(angle_dipole, rho_dipole)
+        dipole = Dipole(rho_dipole, angle_dipole)
         expected_transfer_matrix_h = np.array(
-            [[-0.48338212, 0.91672664], [-0.83595446, -0.48338212]]
+            [[0.5, 1.37832224e02], [-5.44139809e-03, 0.5]]
         )
         expected_transfer_matrix_v = np.array([[1, l_dipole], [0, 1]])
         out = dipole.transfer_matrix()
@@ -32,20 +32,20 @@ class TestDipole(TestCase):
         assert np.allclose(dipole.m_v, out[1])
 
     def test_slice(self):
-        dipole = Dipole(angle_dipole, rho_dipole)
+        dipole = Dipole(rho_dipole, angle_dipole)
         assert len(dipole.slice(10)) == 10
         assert isinstance(dipole.slice(10), Lattice)
         assert np.allclose(dipole.slice(10).m_h, dipole.m_h)
         assert np.allclose(dipole.slice(10).m_v, dipole.m_v)
 
     def test_repr(self):
-        repr(Dipole(angle_dipole, rho_dipole))
+        repr(Dipole(rho_dipole, angle_dipole))
 
     def test_dxztheta_ds(self):
-        dipole = Dipole(angle_dipole, rho_dipole)
+        dipole = Dipole(rho_dipole, angle_dipole)
         # TODO: do the math to check this.
         dipole._dxztheta_ds(0, l_dipole)
 
     def test_plot(self):
-        dipole = Dipole(angle_dipole, rho_dipole)
+        dipole = Dipole(rho_dipole, angle_dipole)
         dipole.plot()
