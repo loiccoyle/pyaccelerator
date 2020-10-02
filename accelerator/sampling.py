5ea1acb04e572d34c21514f39f0a025d1ecbe8f9
Loic Coyle <loic.coyle@hotmail.fr>
2020-10-02 18:23:47 +0200

    add energy spread & dp/p to the match particle distribution
    TODO: make sure the aproach is correct

--- accelerator/sampling.py
+++ accelerator/sampling.py
@@ -5,8 +5,9 @@ import numpy as np
 
 def bigaussian(
     n_particles: int,
-    mean: Tuple[float, float],
+    mean: Tuple[float, float, float],
     geometric_emittance: float,
+    sigma_p: float,
 ) -> np.array:
     """Generate a bigaussian distributed distribution.
 
@@ -18,5 +19,5 @@ def bigaussian(
     Returns:
         Array of position and angle phase space coordinates of the distribution.
     """
-    cov = np.diag((geometric_emittance, geometric_emittance))
+    cov = np.diag((geometric_emittance, geometric_emittance, sigma_p))
     return np.random.multivariate_normal(mean, cov, n_particles).T
--- tests/test_beam.py
+++ tests/test_beam.py
@@ -10,18 +10,18 @@ class TestBeam(TestCase):
         beam = Beam(emittance=3.5e-6)
         assert beam.emittance_h == 3.5e-6
         assert beam.emittance_v == 3.5e-6
-        assert beam.gamma_relativistic == 6927.628011127815
+        assert beam.gamma_relativistic == 6927.628011131436
         assert beam.beta_relativistic == 0.9999999895816034
-        assert beam.geo_emittance_h == 5.05223437350036e-10
-        assert beam.geo_emittance_v == 5.05223437350036e-10
+        assert beam.geo_emittance_h == 5.052234373497719e-10
+        assert beam.geo_emittance_v == 5.052234373497719e-10
 
         beam = Beam(emittance=(3.5e-6, 2.5e-6))
         assert beam.emittance_h == 3.5e-6
         assert beam.emittance_v == 2.5e-6
-        assert beam.gamma_relativistic == 6927.628011127815
+        assert beam.gamma_relativistic == 6927.628011131436
         assert beam.beta_relativistic == 0.9999999895816034
-        assert beam.geo_emittance_h == 5.05223437350036e-10
-        assert beam.geo_emittance_v == 3.608738838214543e-10
+        assert beam.geo_emittance_h == 5.052234373497719e-10
+        assert beam.geo_emittance_v == 3.6087388382126567e-10
 
     def test_ellipse(self):
         beam = Beam()
@@ -42,13 +42,15 @@ class TestBeam(TestCase):
         assert dp.shape == x.shape
 
     def test_match(self):
-        beam = Beam(n_particles=int(1e6))
+        beam = Beam(n_particles=int(1e6), sigma_energy=10)
         x, x_prime, dp = beam.match(twiss=[1, 0, 1])
         self.assertAlmostEqual(x.mean(), 0, places=3)
         self.assertAlmostEqual(x.std(), np.sqrt(beam.geo_emittance_h), places=3)
         self.assertAlmostEqual(x_prime.mean(), 0, places=3)
         self.assertAlmostEqual(x_prime.std(), np.sqrt(beam.geo_emittance_v), places=3)
         assert dp.shape == x.shape
+        self.assertAlmostEqual(dp.max(), beam.sigma_p / beam.p, places=3)
+        self.assertAlmostEqual(dp.min(), beam.sigma_p / beam.p, places=3)
 
         x, x_prime, dp = beam.match(twiss=[1 / 2, 0, 2])
         self.assertAlmostEqual(x.mean(), 0, places=3)
@@ -58,10 +60,16 @@ class TestBeam(TestCase):
             x_prime.std(), np.sqrt(beam.geo_emittance_v * 2), places=3
         )
         assert dp.shape == x.shape
+        self.assertAlmostEqual(dp.max(), beam.sigma_p / beam.p, places=3)
+        self.assertAlmostEqual(dp.min(), beam.sigma_p / beam.p, places=3)
 
     def test_plot(self):
         beam = Beam()
-        beam.plot([1, 0, 1])
+        beam.plot(twiss_h=[1, 0, 1], twiss_v=[0.5, 0, 2])
+        beam.plot(twiss_v=[0.5, 0, 2])
+        beam.plot(twiss_h=[1, 0, 1])
+        with self.assertRaises(ValueError):
+            beam.plot()
 
     def test_repr(self):
         beam = Beam()
