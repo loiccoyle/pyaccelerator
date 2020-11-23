b8d882424d6c7ea779a952fe4a3d3b0f6f83fd45
Loic Coyle <loic.coyle@hotmail.fr>
2020-11-23 15:24:23 +0100

    add kicker

--- pyaccelerator/elements/__init__.py
+++ pyaccelerator/elements/__init__.py
@@ -4,12 +4,14 @@ from .dipole import Dipole, DipoleThin
 from .drift import Drift
 from .quadrupole import Quadrupole, QuadrupoleThin
 from .sextupole import Sextupole, SextupoleThin
+from .kicker import KickerThin
 
 __all__ = [
     "CustomThin",
     "Dipole",
     "DipoleThin",
     "Drift",
+    "KickerThin",
     "Quadrupole",
     "QuadrupoleThin",
     "Sextupole",
